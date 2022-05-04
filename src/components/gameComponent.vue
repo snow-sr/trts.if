e8d4e433d961c499955351f1d8da2b2044f9f92c
snow.sr <joaofelipi877@gmail.com>
2022-05-04 13:11:06 -0300

    game working

---
+++ src/components/gameComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="absolute w-full mt-40 flex flex-col justify-center items-center">
+    <div>
+      <canvas id="canvas" width="240" height="400"></canvas>
+    </div>
+
+    <div id="score" class="mt-10 text-pink-500 text-5xl"></div>
+  </div>
+</template>
