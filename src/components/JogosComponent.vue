6024deb79301cf7c1ee2a50d79f424c0fef21a2a
Davibrlol <daviescolaifc@gmail.com>
2022-05-11 09:35:43 -0300

    Base principal site

---
+++ src/components/JogosComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex justify-center">
+    <ul class="bg-white rounded-lg border border-gray-200 w-96 text-gray-900">
+      <li
+        class="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg bg-blue-600 text-white"
+      >
+        An active item
+      </li>
+      <li class="px-6 py-2 border-b border-gray-200 w-full">A second item</li>
+      <li class="px-6 py-2 border-b border-gray-200 w-full">A third item</li>
+      <li class="px-6 py-2 border-b border-gray-200 w-full">A fourth item</li>
+      <li class="px-6 py-2 w-full rounded-b-lg">And a fifth one</li>
+    </ul>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import App from "./App.vue";
 import Home from "./views/HomeView.vue";
 import Play from "./views/PlayView.vue";
+import Principal from "./views/PrincipalView.vue";
 import "@/assets/base.css";
 import "tw-elements";
 
 const routes = [
   { path: "/", component: Home },
   { path: "/Play", component: Play },
+  { path: "/Principal", component: Principal}
 ];
 
 const router = createRouter({
