d8607a5bbfac307fa1ea7b795f479ac1b3ff0326
João Felipi Cardoso <joaofelipi877@gmail.com>
2022-05-22 13:09:41 -0300

    Build version online

--- server/db/functions.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import "dotenv/config";
-let mongoUri = process.env.URI;
-
-mongoose.connect(mongoUri);
-
-mongoose.Connection.on("Open", () => {
-  console.log("Connected to MongoDB");
-});
